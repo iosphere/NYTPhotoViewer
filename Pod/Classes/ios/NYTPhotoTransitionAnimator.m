e843bbf40322b4b834eb4571377d109becb6c93f
Sebastian Hagedorn <sebastian@iosphere.de>
2015-11-17 17:27:58 +0100

    Removes whiteline to reduce diff to develop

--- Pod/Classes/ios/NYTPhotoTransitionAnimator.m
+++ Pod/Classes/ios/NYTPhotoTransitionAnimator.m
@@ -123,7 +123,6 @@ static const CGFloat NYTPhotoTransitionAnimatorSpringDamping = 0.9;
     
     CGAffineTransform finalEndingViewTransform = self.endingView.transform;
 
-
     CGFloat endingViewInitialTransform = CGRectGetHeight(startingViewForAnimation.frame) / CGRectGetHeight(endingViewForAnimation.frame);
     CGPoint translatedStartingViewCenter = [[self class] centerPointForView:self.startingView
                                                   translatedToContainerView:containerView];
